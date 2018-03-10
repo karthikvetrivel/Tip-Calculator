0ff30850f116f2bd7d7e29196b97100f834cd2b6
KarthikVetrivel <kvvetrivel@gmail.com>
2018-03-10 15:53:54 -0800

    #1

--- Tip Calculator/Tip Calculator/ViewController.swift
+++ Tip Calculator/Tip Calculator/ViewController.swift
@@ -11,17 +11,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var billView: UIView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        billView.layer.cornerRadius = 20
+       
         
-        billView.layer.shadowColor = UIColor.blue.cgColor
-        billView.layer.shadowOffset = CGSize(width: 3, height: 6)
-        billView.layer.shadowOpacity = 1
-        billView.layer.shadowRadius = 4.0
+        
     }
 
     override func didReceiveMemoryWarning() {
